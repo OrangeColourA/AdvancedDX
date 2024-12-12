e3341fc12467c2fe26216f9100441cf7121ce25d
Анатолий Козлов <Anatoliy.kozlov2001@gmail.com>
2024-12-12 11:03:37 +0300

    Created HelloWorld in Win32API

--- AdvancedDX/Main.cpp
+++ AdvancedDX/Main.cpp
@@ -9,7 +9,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     LPSTR lpCmdLine, int nCmdShow)
 {
 
-    MessageBox(NULL, L"Hi SHIT FUCK YOU I HATE YOU MOTHERFUCKONG WOTLD!", L"Hello from message FUCKION G BOX", MB_OK);
+    //MessageBox(NULL, L"Hi SHIT FUCK YOU I HATE YOU MOTHERFUCKONG WOTLD!", L"Hello from message FUCKION G BOX", MB_OK);
 
 
     HWND hMainWnd;
@@ -28,10 +28,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     wc.hInstance      = hInstance;
     wc.hIcon          = LoadIcon(NULL, IDI_QUESTION);
     wc.hCursor        = LoadCursor(NULL, IDC_HAND);
-    wc.hbrBackground  = (HBRUSH)(GetStockObject(BLACK_BRUSH));
+    wc.hbrBackground  = (HBRUSH)(GetStockObject(GRAY_BRUSH));
     wc.lpszMenuName   = NULL;
     wc.lpszClassName  = szClassName;
-    wc.hIconSm        = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIconSm        = LoadIcon(NULL, IDI_QUESTION);
 
     // Registrating motherfuckiong wnd class
 
@@ -42,9 +42,65 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     }
 
 
-    hMainWnd = CreateWindow();
+    hMainWnd = CreateWindow(szClassName, L"CREATED A WINDOW!!!",
+        WS_OVERLAPPEDWINDOW,100,100,300,200,NULL,NULL,hInstance, NULL);
 
+    if (!hMainWnd)
+    {
+        MessageBox(NULL, L"You can't create a fucking window", L"Shit", MB_OK);
+        return 0;
+    }
+
+    
+    ShowWindow(hMainWnd, nCmdShow);
+    // UpdateWindow(hMainWnd); // См примечание в разделе Отображение окна
+
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+
+    return msg.wParam;
+}
+
+
+LRESULT CALLBACK FUCKtoFUCKProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    HDC hDC;
+    PAINTSTRUCT ps;
+    RECT rect;
+
+    switch (uMsg)
+    {
+    case WM_PAINT:
+        hDC = BeginPaint(hWnd, &ps);
+
+        GetClientRect(hWnd, &rect);
+
+        SetBkColor(hDC, 0x0000FFFF);
+        //SetBkMode(hDC, TRANSPARENT);
+
+        DrawText(hDC, L"Congratulation! You have a window!", -1, &rect,
+            DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+        EndPaint(hWnd, &ps);
+        break;
+
+    case WM_CLOSE:
+        DestroyWindow(hWnd);
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
 
 
     return 0;
+
 }
